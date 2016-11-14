2bde12062ddff417b0fc31ee88de933de5510916
Mark Thakerng <southbridge_855@hotmail.com>
2016-11-15 00:53:01 +0700

    upgrade to es-client

--- scripts/mongo-connector.sh
+++ scripts/mongo-connector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MONGO=`ping -c 1 mongo-cluster | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-ES=`ping -c 1 elasticsearch-clients | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+ES=`ping -c 1 es-clients | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 mongo-connector --auto-commit-interval=5 -m ${MONGO}:27017 -t ${ES} -v -d elastic2_doc_manager --stdout
